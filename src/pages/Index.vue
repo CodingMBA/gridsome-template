95eae84aab40207f95c06c12b2dfde0c7094fb87
Andrew Smith <codingmba@gmail.com>
2019-09-15 19:11:44 -0400

    darkens gradient over hero image

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -58,8 +58,8 @@ export default {
 .home-container {
   background-image: linear-gradient(
       to bottom,
-      rgba(0, 0, 0, 0.3),
-      rgba(0, 0, 0, 0.5)
+      rgba(0, 0, 0, 0.5),
+      rgba(0, 0, 0, 0.6)
     ),
     url("~@/assets/images/hero.jpg");
   height: calc(100vh - 146px);
